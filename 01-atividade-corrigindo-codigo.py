379009c3b75f01b41dd3893b5d6e3e80ae22cb03
ArthurFr10 <arthurfragabbmp@gmail.com>
2024-09-17 01:58:12 +0000

    salvar

--- 01-atividade-corrigindo-codigo.py
+++ 01-atividade-corrigindo-codigo.py
@@ -9,6 +9,7 @@ menor_numero = 0
 for i in range(QUANTIDADE_NUMERO):
     numero = int(input(f"Digite o {i +1}ª número: "))
     lista_numeros.append(numero)
+    numeros_invertidos = len(lista_numeros)
 
 
 
@@ -24,22 +25,54 @@ def pares_impares(a):
             contador_impar += 1
     return contador_impar, contador_par
 
-cont_impar, cont_par = pares_impares(lista_numeros)
 
 def media(a):
+    for numero in a:
+        if numero != 0:
+            soma = sum(a)
+            media = soma / QUANTIDADE_NUMERO
+            return media
+        else:
+            return 0
 
+def media_par_impar(a):
+    par = 0
+    impar = 0
+    quantidade_par = []
+    quantidade_impar = []
     for numero in a:
         if numero % 2 == 0:
-            soma1 = sum(lista_numeros)
-            media_par = soma1 / cont_par
+            par += 1
+            quantidade_par.append(numero)
         else:
-            soma2 = sum(lista_numeros)
-            media_impar = soma2 / cont_impar
-    return media_par, media_impar
+            impar += 1
+            quantidade_impar.append(numero)
+    return par, impar, quantidade_par, quantidade_impar
 
-med_par, med_impar = media(lista_numeros)
+def positivo_negativo(a):
+    contador_postivo = 0
+    contador_negativo = 0
+    for numero in a:
+        if numero >= 0:
+            contador_positivo =+ 1
+        else:
+            contador_negativo += 1
+    return contador_positivo, contador_negativo
 
-numeros_invertidos = reversed(lista_numeros)
+def traco():
+    print("==" * 18)
+    return print
+
+
+# Atribuindo valores as variáveis
+
+cont_impar, cont_par = pares_impares(lista_numeros)
+media_total = media(lista_numeros)
+par, impar, quantidade_par, quantidade_impar = media_par_impar(lista_numeros)
+contador_positivo, contador_negativo = positivo_negativo(lista_numeros)
+media_par = media(quantidade_par)
+media_impar = media(quantidade_impar)
+
 maior_numero = max(lista_numeros)
 menor_numero = min(lista_numeros)
 
@@ -51,14 +84,20 @@ menor_numero = min(lista_numeros)
 
 
 
-print("\nEstatísticas dos números:")
+print("\n===== Estatísticas dos números =====")
 print(f"Quantidade de pares: {cont_par}")
 print(f"Quantidade de ímpares: {cont_impar}")
-print(f"Quantidade de positivos: ")
-print(f"Quantidade de negativos: ")
+print(f"Quantidade de positivos: {contador_positivo}")
+print(f"Quantidade de negativos: {contador_negativo}")
+print(f"Quantidade de números inseridos: {QUANTIDADE_NUMERO}")
+traco()
 print(f"Maior número: {maior_numero}")
 print(f"Menor número: {menor_numero}")
-print(f"Média dos números pares: {med_par}")
-print(f"Média dos números ímpares: {med_impar}")
-print(f"Média de todos os números: ")
-print(f"Números na ordem inversa: {numeros_invertidos} ")
+traco()
+print(f"Média dos números pares: {media_par}")
+print(f"Média dos números ímpares: {media_impar}")
+print(f"Média de todos os números: {media_total}")
+traco()
+print(f"Todos os números em ordem inversa: ")
+for i, numero in enumerate(reversed(lista_numeros)):
+    print(f"{QUANTIDADE_NUMERO - i}ª Número: {numero}")
